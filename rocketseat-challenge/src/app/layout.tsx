9f351566f6f9da08344410a00f8e67baef4e8b48
Marcele Monteiro <marcelepmonteiro@gmail.com>
2023-06-15 14:57:31 -0300

    feat: configure styled-components in next.js

--- rocketseat-challenge/src/app/layout.tsx
+++ rocketseat-challenge/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css'
+import StyledComponentsRegistry from './registry'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -15,7 +16,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-BR">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+      </body>
     </html>
   )
 }
